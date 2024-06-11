3984c01c845bfdd0cba98efecba31a1356a024c4
DiahDSyntia <83937381+DiahDSyntia@users.noreply.github.com>
2024-06-11 08:27:39 +0700

    Update tugashipertensi.py

--- tugashipertensi.py
+++ tugashipertensi.py
@@ -192,7 +192,7 @@ if selected == "Modelling":
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
     # Inisialisasi model SVM sebagai base estimator
-    model = SVC(kernel='rbf', C=1, decision_function_shape='ovo')
+    model = SVC(kernel='linear', C=1, decision_function_shape='ovo')
 
     # K-Fold Cross Validation
     k_fold = KFold(n_splits=5, shuffle=True, random_state=0)
